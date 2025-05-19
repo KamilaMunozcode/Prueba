f35e3893c1aef117105e2c8c38203ad54c860396
KamilaMunozcode <johana.m.g1021@gmail.com>
2025-05-19 11:17:10 -0500

    other-commit

--- ejercicios.py
+++
@@ -1,4 +0,0 @@
-numero=float(input("Ingrese un número positivo"))
-print("Hola a todos")
-print("Hola y chao")
-   
--- while.py
+++
@@ -1,9 +0,0 @@
-""" numero = 10
-n = 100
-while (numero < n ):
-    numero = int(input("ingrese un numero: "))
-    if numero != 10 :
-        print("sigue intentando")
-    elif numero == 10:
-        print("ganaste")
-        break  """
